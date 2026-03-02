507f137138d4f5368de3b21c10c4f193a1b135c8
Aryansharma-aryan <157520977+Aryansharma-aryan@users.noreply.github.com>
2026-03-03 03:08:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "https://caialsnew.vercel.app/",
-  "https://www.caials.in/"
+  "https://caialsnew.vercel.app",
+  "https://www.caials.in"
 ];
 
 if (process.env.NODE_ENV !== 'production') {
